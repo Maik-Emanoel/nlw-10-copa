63387ce6bc660d523d09d34e5afcd35beb9f49b4
Maik Emanoel <maikemanoel11@gmail.com>
2022-11-16 09:54:12 -0300

    adjusting games

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function createGame(team1, hour, team2) {
   return `
     <li>
-        <img src="./assets/icon-${team1}.svg" alt="Bandeira do ${team1}">
+        <img src="./assets/icon-${team1}.svg" alt="Bandeira do ${team1}" title="${team1}">
         <strong>${hour}</strong>      
-        <img src="./assets/icon-${team2}.svg" alt="Bandeira da ${team2}">
+        <img src="./assets/icon-${team2}.svg" alt="Bandeira da ${team2}" title="${team2}">
     </li>
   `
 }
@@ -27,15 +27,14 @@ document.querySelector("#cards").innerHTML =
   createCard(
     "24/11", 
     "quinta",
-     createGame("brazil", "18:00", "serbia")) +
+     createGame("brazil", "16:00", "serbia")) +
   createCard(
     "28/11",
     "segunda",
-    createGame("brazil", "13:00", "switzerland") +
-      createGame("portugal", "16:00", "uruguay")
+    createGame("brazil", "13:00", "switzerland")
   ) +
   createCard(
     "02/12", 
     "sexta", 
-    createGame("brazil", "16:00", "cameroon"))
+    createGame("cameroon", "16:00", "brazil"))
 
